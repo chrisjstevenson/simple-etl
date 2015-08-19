30c7b19089e55a7d0f088beefd64096de9da81df
Chris Stevenson <chris.j.stevenson@gmail.com>
2015-08-19 15:18:24 -0500

    initial commit

---
+++ src/db.js
@@ -0,0 +1,149 @@
+var sql = require('mssql'),
+    assert = require('assert'),
+    logger = require('./logger'),
+    config = require('./config');
+
+
+exports.connect = function (callback) {
+
+    var connection = new sql.Connection(config.db, function (err) {
+        if (err) {
+            logger.error(err);
+        }
+        else {
+            logger.info('MSSQL: Connection established on ' + config.db.server + ',' + config.db.port);
+            logger.info('MSSQL: Max pools set to ' + config.db.pool.max);
+            callback(connection);
+        }
+    });
+};
+
+
+exports.promiseTest = function () {
+
+    var connection = new sql.Connection(config.db);
+    connection.connect().then(function () {
+        var request = new sql.Request(connection);
+        request.query('select 1 as number').then(function (recordset) {
+
+            logger.info(recordset);
+            logger.info('executed');
+
+        }).catch(function (err) {
+            logger.error(err);
+        });
+    }).catch (function (err) {
+        logger.error(err);
+    });
+};
+
+
+exports.getTrendData = function () {
+
+    var query = 'SELECT top 100' +
+        'ti.TradeInID,' +
+        'l.LotID,' +
+        'pc.serialnumber,' +
+        'sku.sku,' +
+        'pr.ProfileName,' +
+        'sku.Dept,' +
+        'sku.Class,' +
+        'sku.SubClass,' +
+        'l.InvType,' +
+        'l.ProdcondID,' +
+        'pc.visibleCondID,' +
+        'pc.InOriginalBox,' +
+        'Days_In_Warehouse = DATEDIFF(DAY, ti.DateQuoted, ih.ShipDate),' +
+        'p.PaymentAmount,' +
+        'ti.QuoteTotal,' +
+        //'ti.TestedTotal,' +
+        'ii.SalePrice,' +
+        'ih.ShipDate ' +
+    'FROM dbo.AM_Lots AS l ' +
+    'LEFT OUTER JOIN dbo.AM_InvoiceItems AS ii ON l.LotID = ii.LotID ' +
+    'LEFT OUTER JOIN dbo.AM_InvoiceHeader AS ih ON ii.InvoiceID = ih.InvoiceID ' +
+    'LEFT OUTER JOIN dbo.AM_TradeInLots AS tl ON l.LotID = tl.LotID ' +
+    'LEFT OUTER JOIN dbo.AM_TradeIn AS ti ON tl.TradeInID = ti.TradeInID ' +
+    'LEFT OUTER JOIN dbo.AM_TradeInPayments AS p ON ti.TradeInID = p.TradeInID ' +
+    'INNER JOIN dbo.BBSKUData sku ON l.ConsignorSKU = sku.SKU ' +
+    'LEFT OUTER JOIN dbo.AM_Profile pr ON l.ProfileID = pr.ProfileID ' +
+    'LEFT JOIN AM_ProductCondition pc ON l.LotID = pc.LotID ' +
+    'WHERE ' +
+    "ti.DateQuoted > '1/1/2015'" +
+    'AND ii.SalePrice IS NOT NULL';
+
+    return query;
+}
+
+
+exports.createLotIdQuery = function (lotId) {
+
+    var query = 'SELECT '+
+        'ti.TradeInID, '+
+        'pr.ProfileName as profileName, '+
+        'Days_In_Warehouse = DATEDIFF(DAY, ti.DateQuoted, ih.ShipDate), '+
+        'ti.DateReceived as dateReceived, '+
+        'l.LotID, '+
+        'sku.sku,'+
+        'sku.title, '+
+        'ti.TradeInID, '+
+        'ti.DateQuoted,   '+
+        'p.PaymentAmount,    '+
+        'ti.QuoteTotal,  '+
+        'ti.TestedTotal,  '+
+        'ii.SalePrice,   '+
+        'ih.ShipDate '+
+        'FROM dbo.AM_Lots AS l '+
+        'LEFT OUTER JOIN dbo.AM_InvoiceItems AS ii ON l.LotID = ii.LotID '+
+        'LEFT OUTER JOIN dbo.AM_InvoiceHeader AS ih ON ii.InvoiceID = ih.InvoiceID '+
+        'LEFT OUTER JOIN dbo.AM_TradeInLots AS tl ON l.LotID = tl.LotID '+
+        'LEFT OUTER JOIN dbo.AM_TradeIn AS ti ON tl.TradeInID = ti.TradeInID '+
+        'LEFT OUTER JOIN dbo.AM_TradeInPayments AS p ON ti.TradeInID = p.TradeInID '+
+        'LEFT OUTER JOIN dbo.BBSKUData sku ON l.ConsignorSKU = sku.SKU '+
+        'LEFT OUTER JOIN dbo.AM_Profile pr ON l.ProfileID = pr.ProfileID '+
+        'WHERE l.LotID = ' + lotId;
+
+    return query;
+
+};
+
+
+exports.fetch = function (connection, lotId, callback) {
+
+    var query = 'SELECT '+
+        'ti.TradeInID, '+
+        'pr.ProfileName, '+
+        'Days_In_Warehouse = DATEDIFF(DAY, ti.DateQuoted, ih.ShipDate), '+
+        'ti.DateReceived, '+
+        'l.LotID, '+
+        'sku.sku,'+
+        'sku.title, '+
+        'ti.TradeInID, '+
+        'ti.DateQuoted,   '+
+        'p.PaymentAmount,    '+
+        'ti.QuoteTotal,  '+
+        'ti.TestedTotal,  '+
+        'ii.SalePrice,   '+
+        'ih.ShipDate '+
+        'FROM dbo.AM_Lots AS l '+
+        'LEFT OUTER JOIN dbo.AM_InvoiceItems AS ii ON l.LotID = ii.LotID '+
+        'LEFT OUTER JOIN dbo.AM_InvoiceHeader AS ih ON ii.InvoiceID = ih.InvoiceID '+
+        'LEFT OUTER JOIN dbo.AM_TradeInLots AS tl ON l.LotID = tl.LotID '+
+        'LEFT OUTER JOIN dbo.AM_TradeIn AS ti ON tl.TradeInID = ti.TradeInID '+
+        'LEFT OUTER JOIN dbo.AM_TradeInPayments AS p ON ti.TradeInID = p.TradeInID '+
+        'LEFT OUTER JOIN dbo.BBSKUData sku ON l.ConsignorSKU = sku.SKU '+
+        'LEFT OUTER JOIN dbo.AM_Profile pr ON l.ProfileID = pr.ProfileID '+
+        'WHERE l.LotID = ' + lotId;
+    //logger.debug(query);
+
+    var request = new sql.Request(connection);
+    request.query(query, function(err, recordset) {
+        if (err) {
+            logger.error(err);
+        }
+        else {
+            //logger.info(recordset);
+            callback(recordset);
+        }
+    });
+}
---
+++ tests/db.spec.js
@@ -0,0 +1,207 @@
+var db = require('./../src/db'),
+    expect = require('expect'),
+    sql = require('mssql'),
+    config = require('./../src/config');
+    _ = require('lodash');
+
+
+describe('Tests for db.js:', function () {
+
+    it('should connect to target database', function (done) {
+
+        //db.connect(function(connectionInfo) {
+        //    expect(connectionInfo).toNotBe(null);
+        //    done();
+        //});
+        var connection = new sql.Connection(config.connections.targetDb);
+        connection.connect().then(function () {
+            expect(connection).toNotBe(null);
+            done();
+        });
+    });
+
+    it('should connect to destination database', function(done) {
+
+        var connection = new sql.Connection(config.connections.destinationDb);
+        connection.connect().then(function () {
+            expect(connection).toNotBe(null);
+            done();
+        }).catch(function (err) {
+            logger.error(err);
+        });;
+    });
+
+
+    it('should do this and then that', function (done) {
+
+        var connection = new sql.Connection(config.connections.targetDb);
+        connection.connect().then(function () {
+            var request = new sql.Request(connection);
+            var query = db.getTrendData();
+
+            request.query('SELECT 1 AS NUMBER;').then(function (recordset) {
+                expect(recordset[0]).toNotBeNull;
+                done();
+            }).catch(function (err) {
+                logger.error(err);
+            });
+        }).catch(function (err) {
+            logger.error(err);
+        });
+    });
+
+
+    it('should use streaming', function(done) {
+        this.timeout(10000);
+        var connection = new sql.Connection(config.connections.targetDb);
+        connection.connect().then(function () {
+            var request = new sql.Request(connection);
+            request.stream = true;
+            request.query(db.getTrendData());
+
+            request.on('recordset', function(columns) {
+                //console.log(columns)
+            });
+
+            request.on('row', function(row) {
+                expect(row.ProfileName).toBeA('string')
+                expect(row.LotID).toBeA('number');
+                //console.log(row);
+            });
+
+            request.on('error', function(err) {
+                console.log(err);
+            });
+
+            request.on('done', function(returnValue) {
+                done();
+            });
+
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+
+
+    it('should create a table if it does not exist', function(done) {
+
+        var connection = new sql.Connection(config.connections.destinationDb);
+        connection.connect().then(function () {
+
+            var table = new sql.Table('Products');
+            table.create = true;
+            table.columns.add('a', sql.Int, {nullable: true});
+            table.columns.add('b', sql.VarChar(50), {nullable: false});
+            table.rows.add(777, 'test');
+
+            var request = new sql.Request(connection);
+            request.bulk(table, function(err, rowCount) {
+                if (err) console.log(err);
+                done();
+            });
+
+
+        }).catch(function (err) {
+            console.log(err);
+        });;
+    });
+
+
+    it('should create a table column from a recordset column definition', function(done) {
+
+        var definition = {
+            index: 3,
+            name: 'sku',
+            length: 4,
+            type: [sql.Int],
+            scale: undefined,
+            precision: undefined,
+            nullable: true,
+            caseSensitive: false,
+            identity: false,
+            readOnly: false
+        }
+
+        var connection = new sql.Connection(config.connections.destinationDb);
+        connection.connect().then(function () {
+
+            var table = new sql.Table('Trend_UnitTest');
+            table.create = true;
+            table.columns.add(definition.name, definition.type[0], { nullable: definition.nullable });
+            table.rows.add(77773434);
+
+            var request = new sql.Request(connection);
+            request.bulk(table, function(err, rowCount) {
+                if (err) console.log(err);
+                done();
+            });
+
+
+        }).catch(function (err) {
+            console.log(err);
+        });;
+    });
+
+
+    it('should create a table in a destination db with recordset columns', function (done) {
+
+        this.timeout(10000);
+        var table = undefined;
+
+        var connection = new sql.Connection(config.connections.targetDb);
+        connection.connect().then(function () {
+            var request = new sql.Request(connection);
+            request.stream = true;
+            request.query(db.getTrendData());
+
+            request.on('recordset', function(columns) {
+
+                table = new sql.Table('Dynamictable_UnitTest');
+                table.create = true;
+
+                var arr = _.values(columns)
+                arr.forEach(function(def) {
+                    table.columns.add(def.name, def.type, { nullable: def.nullable } );
+                });
+            });
+
+            request.on('error', function(err) {
+                console.log(err);
+            });
+
+            request.on('done', function(returnValue) {
+            });
+
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+
+        var destinationConnection = new sql.Connection(config.connections.destinationDb);
+        destinationConnection.connect().then(function () {
+
+            //console.log(table);
+
+            var createTableRequest = new sql.Request(destinationConnection);
+            createTableRequest.bulk(table, function(err, rowCount) {
+                if (err) console.log(err);
+                done();
+            });
+
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+
+    });
+});
+
+
+
+// Notes:
+// 1.) Call done() after your test assertions.
+// 2.)
+
+
+
+
